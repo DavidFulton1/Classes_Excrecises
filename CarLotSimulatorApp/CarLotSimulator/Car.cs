46a67777aa02604ae36fe5610896b6a09ecc6e82
DavidFulton1 <fultondavids@gmail.com>
2022-07-14 10:25:21 -0500

    new commit'

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public Car() 
+        { 
+        }
+        public int Year { get; set; }
+        public String Make { get; set; }
+        public String Model { get; set; }
+        public bool IsDrivable { get; set; }
+        public String EngineNoise { get; set; }
+        public String HonkNoise { get; set; }
+    
+    
+    public static void MakeEngineNoise(string EngineNoise)
+        {if (EngineNoise=="cool")
+            {
+                Console.WriteLine("VVVVRRROOOOOOOOOOM");
+            }
+
+            else
+            {
+                Console.WriteLine("sad vroom");
+            }
+        }
+    
+    public static void MakeHonkNoise(string HonkNoise)
+        {if (HonkNoise == "cool")
+            {
+                Console.WriteLine("La Cucaracha");
+            }
+            else
+            {
+                Console.WriteLine("BEEP");
+            }
+
+        }
+    
+    
+    }
+
+
+
+
+
+
+
+
+}
+
+
