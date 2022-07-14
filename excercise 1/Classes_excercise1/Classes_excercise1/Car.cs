cd3a77a7d451e78b683ba3b5c5e730c30a4b054c
DavidFulton1 <fultondavids@gmail.com>
2022-07-14 10:20:29 -0500

    excercises1+2

---
+++ excercise 1/Classes_excercise1/Classes_excercise1/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_excercise1
+{
+    public class Car
+    {
+        public Car()
+        { 
+        }
+        public String Make { get; set; }
+
+        public String Model { get; set; }
+        public int Year { get; set; }
+
+      
+     }
+
+
+}
+
+    
+
+
---
+++ excercise 1/Classes_excercise1/Classes_excercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Classes_excercise1;
+Car car1 = new Car()
+{
+    Make = "Ford",
+    Model = "Ranger",
+Year = 2002
+};
+Car car2 = new Car()
+{
+    Make = "Honda",
+    Model = "Civic",
+    Year =2004
+};
+Car car3 = new Car()
+{
+    Make = "Chevy",
+    Model = "Corvette",
+    Year = 2016
+};
+
+Console.WriteLine($"{car1.Make}- {car1.Model}- {car1.Year}" );
